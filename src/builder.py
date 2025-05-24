4506926de116634deb0a94fae49e33ac3ff68c85
Dinarchak <jjustdinar@gmail.com>
2025-05-24 21:43:37 +0300

    refactor реализовать генерацию выходных файлов через билдер

--- src/builder.py
+++ src/builder.py
@@ -4,31 +4,46 @@ from xml_utils import create_xml_tree, prettyfy_xml
 import json
 
 
-def build_xml_config(path: str, root: Object):
-    """генерирует внутреннюю конфигурацию базовой станции
-    в config.xml
+class ConfigBuilder:
+    def __init__(self, root: Object, classes: dict[str, Object], config_path: str, meta_path: str):
+        """Класс для генерации выходных файлов
 
-    Args:
-        path (str): путь к файлу
-        root (Object): корневой объект
-    """
-    root_xml = root.create_xml_element(None)
-    create_xml_tree(root_xml, root.children)
-
-    pretty_xml = prettyfy_xml(root_xml)
-
-    with open(path, "w", encoding="utf-8") as f:
-        f.write(pretty_xml)
+        Args:
+            root (Object): корневой класс
+            config_path (str): путь до config.xml
+            meta_path (str): путь до meta.json
+        """
+        self.root: Object = root
+        self.classes: dict[str, Object] = classes
+        self.config_path: str = config_path
+        self.meta_path: str = meta_path
 
 
-def build_json_meta(path: str, classes: dict[str, Object]):
-    """Генерирует файл meta.json с
-    мета-информацию о классах и их атрибутах
+    def build_xml_config(self):
+        """генерирует внутреннюю конфигурацию базовой станции
+        в config.xml
 
-    Args:
-        path (str): путь к файлу
-        classes (dict[str, Object]): словарь классов(ключ - тег класса, значение - экземпляр класса)
-    """
-    json_list = [cls_obj.to_dict() for cls_obj in classes.values() if not cls_obj.isRoot]
-    with open(path, 'w', encoding='utf-8') as f:
-        json.dump(json_list, f, indent=4)
+        Args:
+            path (str): путь к файлу
+            root (Object): корневой объект
+        """
+        root_xml = self.root.create_xml_element(None)
+        create_xml_tree(root_xml, self.root.children)
+
+        pretty_xml = prettyfy_xml(root_xml)
+
+        with open(self.config_path, "w", encoding="utf-8") as f:
+            f.write(pretty_xml)
+
+
+    def build_json_meta(self):
+        """Генерирует файл meta.json с
+        мета-информацию о классах и их атрибутах
+
+        Args:
+            path (str): путь к файлу
+            classes (dict[str, Object]): словарь классов(ключ - тег класса, значение - экземпляр класса)
+        """
+        json_list = [cls_obj.to_dict() for cls_obj in self.classes.values() if not cls_obj.isRoot]
+        with open(self.meta_path, 'w', encoding='utf-8') as f:
+            json.dump(json_list, f, indent=4)
--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
 from parser import parse_input_xml
-from builder import build_xml_config, build_json_meta
+from builder import ConfigBuilder
     
 
 def run():
     classes, root_cls_obj = parse_input_xml('./input/test_input.xml')
-    build_xml_config("./out/config.xml", root_cls_obj)
-    build_json_meta('./out/meta.json', classes)
+    builder = ConfigBuilder(
+        root=root_cls_obj,
+        classes=classes,
+        config_path='./out/config.xml',
+        meta_path='./out/meta.json'
+    )
+    builder.build_xml_config()
+    builder.build_json_meta()
 
 
 if __name__ == '__main__':
